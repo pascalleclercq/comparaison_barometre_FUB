f636268572b882f3da9780726e5e9914997145cf
CyclisteBx <sped-seldom-0z@icloud.com>
2025-06-01 17:48:32 +0200

    Ajout des medailles pour les 4 meilleures progressions dans le fichier tweets.txt

--- barometre_dep_2025.py
+++ barometre_dep_2025.py
@@ -4,6 +4,7 @@ from io import StringIO
 import geopandas as gpd
 import matplotlib as mpl
 import matplotlib.pyplot as pl
+import numpy as np
 import pandas as pd
 import requests  # library to handle requests
 from bs4 import BeautifulSoup  # library to parse HTML documents
@@ -72,6 +73,7 @@ qualifs_2025 = []
 
 qualifs = pd.DataFrame(
     columns=["qualifs_2021", "qualifs_2025", "diff"],
+    dtype=float,
     index=dep_ids,
 )
 
@@ -146,237 +148,213 @@ ax[0].set_aspect(1.3)
 ax[1].set_aspect(1.3)
 pl.savefig("png/{}.png".format("France_metropolitaine"), dpi=300)
 
-dep_ids.remove('75')
-# dep_ids.remove('92')
-# dep_ids.remove('93')
-# dep_ids.remove('94')
-# dep_ids.remove('95')
-# dep_ids.remove('974')
+
+
+for idep in tqdm(dep_ids):
+    dep_id = idep
+    is_dep = gdf_communes["insee"].str.startswith(str(dep_id))
+    gdf_dep = gdf_communes[is_dep]
+
+    lon_min, lat_min, lon_max, lat_max = gdf_dep.total_bounds
+    ratio_dep = (lat_max - lat_min) / (lon_max - lon_min)
+    fig, ax = pl.subplots(
+        1, 2, constrained_layout=True, figsize=(10, 10 * ratio_dep + 2)
+    )
+    fig.patch.set_facecolor((0.50, 0.51, 0.55))
+
+    with contextlib.suppress(ValueError):
+        gdf_dep[gdf_dep["contributions_2021"] == 0].plot(
+            color=(0.58, 0.59, 0.63),
+            ax=ax[0],
+            edgecolor=(0.65, 0.67, 0.69),
+        )
+    with contextlib.suppress(ValueError):
+        gdf_dep[
+            (gdf_dep["contributions_2021"] > 0) & (gdf_dep["contributions_2021"] < 50)
+        ].plot(
+            color=(0.77, 0.18, 0.20),
+            ax=ax[0],
+            edgecolor=(0.65, 0.67, 0.69),
+        )
+    with contextlib.suppress(ValueError):
+        gdf_dep[(gdf_dep["contributions_2021"] >= 50)].plot(
+            color=(0.61, 0.87, 0.19),
+            ax=ax[0],
+            edgecolor=(0.65, 0.67, 0.69),
+        )
+
+
+    with contextlib.suppress(ValueError):
+        gdf_dep[gdf_dep["contributions_2025"] == 0].plot(
+            color=(0.58, 0.59, 0.63),
+            ax=ax[1],
+            edgecolor=(0.65, 0.67, 0.69),
+        )
+    with contextlib.suppress(ValueError):
+        gdf_dep[
+            (( gdf_dep["contributions_2025"] < 50) & (gdf_dep["population"] > 5000)) | ((gdf_dep["contributions_2025"] < 30) & (gdf_dep["population"] <= 5000))
+        ].plot(
+            color=(0.77, 0.18, 0.20),
+            ax=ax[1],
+            edgecolor=(0.65, 0.67, 0.69),
+        )
+    with contextlib.suppress(ValueError):
+        gdf_dep[(( gdf_dep["contributions_2025"] >= 50) & (gdf_dep["population"] > 5000)) | ((gdf_dep["contributions_2025"] >= 30) & (gdf_dep["population"] <= 5000))].plot(
+            color=(0.61, 0.87, 0.19),
+            ax=ax[1],
+            edgecolor=(0.65, 0.67, 0.69),
+        )
+
+    qualif_2021 = len(gdf_dep[(gdf_dep["contributions_2021"] >= 50)])
+    qualif_2025 = len(gdf_dep[(( gdf_dep["contributions_2025"] >= 50) & (gdf_dep["population"] > 5000)) | ((gdf_dep["contributions_2025"] >= 30) & (gdf_dep["population"] <= 5000))])
+    sup_zero_2021 = len(gdf_dep[(gdf_dep["contributions_2021"] > 0)])
+    sup_zero_2025 = len(gdf_dep[(gdf_dep["contributions_2025"] > 0)])
+
+    qualifs_2021.append(qualif_2021)
+    qualifs_2025.append(qualif_2025)
+
+    qualifs.loc[dep_id, 'qualifs_2021'] = qualif_2021
+    qualifs.loc[dep_id, 'qualifs_2025'] = qualif_2025
+    qualifs.loc[dep_id, 'diff'] = qualif_2025 - qualif_2021
+    ax[0].set_title(
+        " Participation au Barometre Vélo : {:}\n\n  2021".format(
+            df_dep[df_dep["N° du dép."] == dep_id][
+                "Nom du département"
+            ].values[0]
+            # "Ile de France"
+        ),
+        loc="left",
+        fontdict={"fontsize": "24"},
+    )
+    ax[1].set_title("\n\n  2025", loc="left", fontdict={"fontsize": "24"})
+
+    ax[0].set_axis_off()
+    ax[1].set_axis_off()
+
+    box_text_communes = TextArea(
+        f"Communes : {len(gdf_dep)} ", textprops=dict(color="w", size=16)
+    )
+    box_text_red = TextArea(
+        f" au moins une réponse : {sup_zero_2021} ",
+        textprops=dict(color="w", size=16),
+    )
+    box_draw_red = DrawingArea(20, 20, 0, 0)
+    rect = Rectangle((2, 2), width=16, height=16, angle=0, fc=(0.77, 0.18, 0.20))
+    box_draw_red.add_artist(rect)
+    box_red = HPacker(
+        children=[box_draw_red, box_text_red],
+        align="center",
+        pad=0,
+        sep=0,
+        mode="fixed",
+    )
+
+    box_text_green = TextArea(
+        f" qualifiées : {qualif_2021} ", textprops=dict(color="w", size=16)
+    )
+    box_draw_green = DrawingArea(20, 20, 0, 0)
+    rect = Rectangle((2, 2), width=16, height=16, angle=0, fc=(0.61, 0.87, 0.19))
+    box_draw_green.add_artist(rect)
+    box_green = HPacker(
+        children=[box_draw_green, box_text_green],
+        align="center",
+        pad=0,
+        sep=0,
+        mode="fixed",
+    )
+
+    box = VPacker(
+        children=[box_text_communes, box_green, box_red],
+        align="left",
+        pad=0,
+        sep=4,
+        mode="fixed",
+    )
+    anchored_box = AnchoredOffsetbox(
+        loc="upper center",
+        child=box,
+        pad=0.0,
+        frameon=False,
+        bbox_to_anchor=(0.4, 0),
+        bbox_transform=ax[0].transAxes,
+        borderpad=0.0,
+    )
+    ax[0].add_artist(anchored_box)
+
+    box_text_communes = TextArea(
+        f"Communes : {len(gdf_dep)} ", textprops=dict(color="w", size=16)
+    )
+    box_text_red = TextArea(
+        f" au moins une réponse : {sup_zero_2025} ",
+        textprops=dict(color="w", size=16),
+    )
+    box_draw_red = DrawingArea(20, 20, 0, 0)
+    rect = Rectangle((2, 2), width=16, height=16, angle=0, fc=(0.77, 0.18, 0.20))
+    box_draw_red.add_artist(rect)
+    box_red = HPacker(
+        children=[box_draw_red, box_text_red],
+        align="center",
+        pad=0,
+        sep=0,
+        mode="fixed",
+    )
+
+    box_text_green = TextArea(
+        f" qualifiées : {qualif_2025} ", textprops=dict(color="w", size=16)
+    )
+    box_draw_green = DrawingArea(20, 20, 0, 0)
+    rect = Rectangle((2, 2), width=16, height=16, angle=0, fc=(0.61, 0.87, 0.19))
+    box_draw_green.add_artist(rect)
+    box_green = HPacker(
+        children=[box_draw_green, box_text_green],
+        align="center",
+        pad=0,
+        sep=0,
+        mode="fixed",
+    )
+
+    box = VPacker(
+        children=[box_text_communes, box_green, box_red],
+        align="left",
+        pad=0,
+        sep=4,
+        mode="fixed",
+    )
+    anchored_box = AnchoredOffsetbox(
+        loc="upper center",
+        child=box,
+        pad=0.0,
+        frameon=False,
+        bbox_to_anchor=(0.4, 0),
+        bbox_transform=ax[1].transAxes,
+        borderpad=0.0,
+    )
+    ax[1].add_artist(anchored_box)
+    pl.savefig(f"png/{dep_id}.png", dpi=300)
+
+    qualifs["diff"] = qualifs["qualifs_2025"] - qualifs["qualifs_2021"]
+    try:
+        qualifs["relatif"] = qualifs["qualifs_2025"].divide(qualifs["qualifs_2021"])
+    except ZeroDivisionError:
+        qualifs["relatif"] = np.nan
+    qualifs.to_csv('outputs/commune_qualif.csv')
 
 with open("outputs/tweets.txt", "w") as f:
     for idep in tqdm(dep_ids):
         dep_id = idep
-
-        # Eventuellement pour l'Ile de France
-        # for i, dep_id in enumerate(['75', '77', '78', '91', '92', '93', '94', '95']):
-        #     is_dep = gdf_communes["insee"].str.startswith(str(dep_id))
-        #     if i == 0:
-        #         gdf_dep = gdf_communes[is_dep]
-        #     else:
-        #         gdf_dep = pd.concat([gdf_dep, gdf_communes[is_dep]])
-
         is_dep = gdf_communes["insee"].str.startswith(str(dep_id))
         gdf_dep = gdf_communes[is_dep]
-
-        lon_min, lat_min, lon_max, lat_max = gdf_dep.total_bounds
-        ratio_dep = (lat_max - lat_min) / (lon_max - lon_min)
-        fig, ax = pl.subplots(
-            1, 2, constrained_layout=True, figsize=(10, 10 * ratio_dep + 2)
-        )
-        fig.patch.set_facecolor((0.50, 0.51, 0.55))
-
-        with contextlib.suppress(ValueError):
-            gdf_dep[gdf_dep["contributions_2021"] == 0].plot(
-                color=(0.58, 0.59, 0.63),
-                ax=ax[0],
-                edgecolor=(0.65, 0.67, 0.69),
-            )
-        with contextlib.suppress(ValueError):
-            gdf_dep[
-                (gdf_dep["contributions_2021"] > 0) & (gdf_dep["contributions_2021"] < 50)
-            ].plot(
-                color=(0.77, 0.18, 0.20),
-                ax=ax[0],
-                edgecolor=(0.65, 0.67, 0.69),
-            )
-        with contextlib.suppress(ValueError):
-            gdf_dep[(gdf_dep["contributions_2021"] >= 50)].plot(
-                color=(0.61, 0.87, 0.19),
-                ax=ax[0],
-                edgecolor=(0.65, 0.67, 0.69),
-            )
-
-
-        with contextlib.suppress(ValueError):
-            gdf_dep[gdf_dep["contributions_2025"] == 0].plot(
-                color=(0.58, 0.59, 0.63),
-                ax=ax[1],
-                edgecolor=(0.65, 0.67, 0.69),
-            )
-        with contextlib.suppress(ValueError):
-            gdf_dep[
-                (( gdf_dep["contributions_2025"] < 50) & (gdf_dep["population"] > 5000)) | ((gdf_dep["contributions_2025"] < 30) & (gdf_dep["population"] <= 5000))
-            ].plot(
-                color=(0.77, 0.18, 0.20),
-                ax=ax[1],
-                edgecolor=(0.65, 0.67, 0.69),
-            )
-        with contextlib.suppress(ValueError):
-            gdf_dep[(( gdf_dep["contributions_2025"] >= 50) & (gdf_dep["population"] > 5000)) | ((gdf_dep["contributions_2025"] >= 30) & (gdf_dep["population"] <= 5000))].plot(
-                color=(0.61, 0.87, 0.19),
-                ax=ax[1],
-                edgecolor=(0.65, 0.67, 0.69),
-            )
-
-        qualif_2021 = len(gdf_dep[(gdf_dep["contributions_2021"] >= 50)])
-        qualif_2025 = len(gdf_dep[(( gdf_dep["contributions_2025"] >= 50) & (gdf_dep["population"] > 5000)) | ((gdf_dep["contributions_2025"] >= 30) & (gdf_dep["population"] <= 5000))])
-        sup_zero_2021 = len(gdf_dep[(gdf_dep["contributions_2021"] > 0)])
-        sup_zero_2025 = len(gdf_dep[(gdf_dep["contributions_2025"] > 0)])
-
-        qualifs_2021.append(qualif_2021)
-        qualifs_2025.append(qualif_2025)
-
-        qualifs.loc[dep_id, 'qualifs_2021'] = qualif_2021
-        qualifs.loc[dep_id, 'qualifs_2025'] = qualif_2025
-        qualifs.loc[dep_id, 'diff'] = qualif_2025 - qualif_2021
-        ax[0].set_title(
-            " Participation au Barometre Vélo : {:}\n\n  2021".format(
-                df_dep[df_dep["N° du dép."] == dep_id][
-                    "Nom du département"
-                ].values[0]
-                # "Ile de France"
-            ),
-            loc="left",
-            fontdict={"fontsize": "24"},
-        )
-        ax[1].set_title("\n\n  2025", loc="left", fontdict={"fontsize": "24"})
-
-        ax[0].set_axis_off()
-        ax[1].set_axis_off()
-
-        box_text_communes = TextArea(
-            f"Communes : {len(gdf_dep)} ", textprops=dict(color="w", size=16)
-        )
-        box_text_red = TextArea(
-            f" au moins une réponse : {sup_zero_2021} ",
-            textprops=dict(color="w", size=16),
-        )
-        box_draw_red = DrawingArea(20, 20, 0, 0)
-        rect = Rectangle((2, 2), width=16, height=16, angle=0, fc=(0.77, 0.18, 0.20))
-        box_draw_red.add_artist(rect)
-        box_red = HPacker(
-            children=[box_draw_red, box_text_red],
-            align="center",
-            pad=0,
-            sep=0,
-            mode="fixed",
-        )
-
-        box_text_green = TextArea(
-            f" qualifiées : {qualif_2021} ", textprops=dict(color="w", size=16)
-        )
-        box_draw_green = DrawingArea(20, 20, 0, 0)
-        rect = Rectangle((2, 2), width=16, height=16, angle=0, fc=(0.61, 0.87, 0.19))
-        box_draw_green.add_artist(rect)
-        box_green = HPacker(
-            children=[box_draw_green, box_text_green],
-            align="center",
-            pad=0,
-            sep=0,
-            mode="fixed",
-        )
-
-        box = VPacker(
-            children=[box_text_communes, box_green, box_red],
-            align="left",
-            pad=0,
-            sep=4,
-            mode="fixed",
-        )
-        anchored_box = AnchoredOffsetbox(
-            loc="upper center",
-            child=box,
-            pad=0.0,
-            frameon=False,
-            bbox_to_anchor=(0.4, 0),
-            bbox_transform=ax[0].transAxes,
-            borderpad=0.0,
-        )
-        ax[0].add_artist(anchored_box)
-
-        box_text_communes = TextArea(
-            f"Communes : {len(gdf_dep)} ", textprops=dict(color="w", size=16)
-        )
-        box_text_red = TextArea(
-            f" au moins une réponse : {sup_zero_2025} ",
-            textprops=dict(color="w", size=16),
-        )
-        box_draw_red = DrawingArea(20, 20, 0, 0)
-        rect = Rectangle((2, 2), width=16, height=16, angle=0, fc=(0.77, 0.18, 0.20))
-        box_draw_red.add_artist(rect)
-        box_red = HPacker(
-            children=[box_draw_red, box_text_red],
-            align="center",
-            pad=0,
-            sep=0,
-            mode="fixed",
-        )
-
-        box_text_green = TextArea(
-            f" qualifiées : {qualif_2025} ", textprops=dict(color="w", size=16)
-        )
-        box_draw_green = DrawingArea(20, 20, 0, 0)
-        rect = Rectangle((2, 2), width=16, height=16, angle=0, fc=(0.61, 0.87, 0.19))
-        box_draw_green.add_artist(rect)
-        box_green = HPacker(
-            children=[box_draw_green, box_text_green],
-            align="center",
-            pad=0,
-            sep=0,
-            mode="fixed",
-        )
-
-        box = VPacker(
-            children=[box_text_communes, box_green, box_red],
-            align="left",
-            pad=0,
-            sep=4,
-            mode="fixed",
-        )
-        anchored_box = AnchoredOffsetbox(
-            loc="upper center",
-            child=box,
-            pad=0.0,
-            frameon=False,
-            bbox_to_anchor=(0.4, 0),
-            bbox_transform=ax[1].transAxes,
-            borderpad=0.0,
-        )
-        ax[1].add_artist(anchored_box)
-
-        # box_text_title = TextArea("Participation au Barometre Vélo: {:}".format(
-        #         df_dep[df_dep["N° du dép."] == dep_id]["Nom du département"].values[0], textprops=dict(color="w", size=30)
-        # ))
-        #
-        # anchored_box_title = AnchoredOffsetbox(
-        #     loc="upper left",
-        #     child=box_text_title,
-        #     pad=0.0,
-        #     frameon=False,
-        #     bbox_to_anchor=(0, 1.2),
-        #     bbox_transform=ax[0].transAxes,
-        #     borderpad=0,
-        # )
-        #
-        # ax[0].add_artist(anchored_box_title)
-
-        # pl.suptitle(
-        #     "Participation au Barometre Vélo: {:}".format(
-        #         df_dep[df_dep["N° du dép."] == dep_id]["Nom du département"].values[0]
-        #     ),
-        #     x=0.1,
-        #     y=0.98,
-        #     fontsize=22,
-        #     horizontalalignment="left",
-        # )
-        # pl.tight_layout()                                                                                                                      fontdict={'fontsize':'25'}
-        pl.savefig(f"png/{dep_id}.png", dpi=300)
         f.write(
             "Évolution de la participation 2019-2021 au Baromètre Vélo organisé par @FUB_fr\n\n"
         )
+
+
+        qualif_2025 = qualifs.loc[dep_id, "qualifs_2025"]
+        qualif_2021 = qualifs.loc[dep_id, "qualifs_2021"]
+
         if (qualif_2025 - qualif_2021) < -1:
             f.write(
-                "{:}: {} communes qualifiées en moins".format(
+                "{:}: {:0.0f} communes qualifiées en moins".format(
                     df_dep[df_dep["N° du dép."] == dep_id][
                         "Nom du département"
                     ].values[0],
@@ -386,7 +364,7 @@ with open("outputs/tweets.txt", "w") as f:
 
         elif (qualif_2025 - qualif_2021) == -1:
             f.write(
-                "{:}: {} commune qualifiée en moins".format(
+                "{:}: {:0.0f} commune qualifiée en moins".format(
                     df_dep[df_dep["N° du dép."] == dep_id][
                         "Nom du département"
                     ].values[0],
@@ -405,7 +383,7 @@ with open("outputs/tweets.txt", "w") as f:
 
         elif (qualif_2025 - qualif_2021) == 1:
             f.write(
-                "{:}: {} commune qualifiée en plus".format(
+                "{:}: {:0.0f} commune qualifiée en plus".format(
                     df_dep[df_dep["N° du dép."] == dep_id][
                         "Nom du département"
                     ].values[0],
@@ -414,7 +392,7 @@ with open("outputs/tweets.txt", "w") as f:
             )
         else:
             f.write(
-                "{:}: {} communes qualifiées en plus".format(
+                "{:}: {:0.0f} communes qualifiées en plus".format(
                     df_dep[df_dep["N° du dép."] == dep_id][
                         "Nom du département"
                     ].values[0],
@@ -437,15 +415,16 @@ with open("outputs/tweets.txt", "w") as f:
         if ((qualif_2025 - qualif_2021)>51):
             emoji = "👈👏🤯"
 
+        qualifs.sort_values(by='diff', ascending=False, inplace=True)
         medaille = ''
-        # if (qualif_2025 - qualif_2021) == qualifs.iloc[0]['diff']:
-        #     medaille = 'Meilleure progression 🥇'
-        # elif (qualif_2025 - qualif_2021) == qualifs.iloc[1]['diff']:
-        #     medaille = '2ème meilleure progression 🥈'
-        # elif (qualif_2025 - qualif_2021) == qualifs.iloc[2]['diff']:
-        #     medaille = '3ème meilleure progression 🥉'
-        # elif (qualif_2025 - qualif_2021) == qualifs.iloc[3]['diff']:
-        #     medaille = '4ème meilleure progression 🍫'
+        if (qualif_2025 - qualif_2021) == qualifs.iloc[0]['diff']:
+            medaille = 'Meilleure progression 🥇'
+        elif (qualif_2025 - qualif_2021) == qualifs.iloc[1]['diff']:
+            medaille = '2ème meilleure progression 🥈'
+        elif (qualif_2025 - qualif_2021) == qualifs.iloc[2]['diff']:
+            medaille = '3ème meilleure progression 🥉'
+        elif (qualif_2025 - qualif_2021) == qualifs.iloc[3]['diff']:
+            medaille = '4ème meilleure progression 🍫'
 
         f.write(f" {emoji}")
         if medaille:
@@ -454,8 +433,3 @@ with open("outputs/tweets.txt", "w") as f:
         f.write("\n\n\n")
 
         pl.close()
-
-
-qualifs["diff"] = qualifs["qualifs_2025"] - qualifs["qualifs_2021"]
-# qualifs["relatif"] = qualifs["qualifs_2025"] / qualifs["qualifs_2021"]
-qualifs.to_csv('outputs/commune_qualif.csv')
