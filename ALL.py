f227eb5757a2fcf3889795f3f357cd823b0e4f8c
Pascal Leclercq <pascal.leclercq@gmail.com>
2025-07-05 14:29:35 +0200

    feat: merge MEL PPI subdivisions
    add barometre_EPCI_2025_logo.py to ALL.py

--- ALL.py
+++ ALL.py
@@ -8,3 +8,4 @@ for key, value in EPCI.all.items():
     os.system("python barometre_EPCI.py "+key+" 2021 taux")
     os.system("python barometre_EPCI.py "+key+" 2025 reponses")
     os.system("python barometre_EPCI.py "+key+" 2021 reponses")
+    os.system("python barometre_EPCI_2025_logo.py "+key)
